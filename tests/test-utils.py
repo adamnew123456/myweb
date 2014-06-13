7c4ef74c03e71bae6bc98a95b181d7865deeea38
adamnew123456 <adamnew123456@gmail.com>
2014-06-12 21:20:18 -0400

    Corrected some naming errors in the utilities test.

--- tests/test-utils.py
+++ tests/test-utils.py
@@ -6,10 +6,10 @@ import myweb.backend.utils as utils
 
 SAMPLE_TEXT = 'This is a link to [[a]] and [[b]] but not [[]]'
 
-LINKS = utils.get_LINKS(SAMPLE_TEXT)
+LINKS = utils.get_links(SAMPLE_TEXT)
 assert LINKS == {'a', 'b'}
 
-CHUNKS = utils.get_link_CHUNKS(SAMPLE_TEXT)
+CHUNKS = utils.get_link_chunks(SAMPLE_TEXT)
 assert CHUNKS == [
     utils.Text('This is a link to '),
     utils.Link('a'),
